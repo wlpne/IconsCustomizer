e0e33725cc2d213345a5a3d45ad21b1244f33a9e
Ilya <iliavaitkovich@gmail.com>
2026-08-13 23:10:07 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ukm.app.iconscustomizer"
         minSdk = 35
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
